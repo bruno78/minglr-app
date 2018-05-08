eda727cd1ce98050ab81a00e55da12822e684abe
bruno78 <devbruno78@gmail.com>
2018-05-08 18:52:03 -0400

    Add firebase libraries, add Registration Login activity and layout

---
+++ app/src/main/java/com/brunogtavares/minglr/ChooseLoginRegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.brunogtavares.minglr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseLoginRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_registration);
+    }
+}
